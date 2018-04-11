2df44c5abfb35acafae223d19abb12a23a518991
Daniel Everett <deverett@gmail.com>
2018-04-12 06:45:00 +1000

    POST handler for /data route moved to module

---
+++ api-data.js
@@ -0,0 +1,86 @@
+const express = require('express')
+const api = express.Router()
+const Busboy = require('busboy');
+const tempy = require('tempy');
+const fs = require('fs');
+const xlsx = require('xlsx');
+const db = require('./db');
+
+api.post('/:number([1-9]{1})', (req, res) => {
+  var busboy = new Busboy({ headers : req.headers });
+	var book;
+  var writePromises = [];
+  var dataNumber = req.params.number;
+  var fieldMap = null;
+  var fileType = null;
+  var nameField = null;
+	var sheetName = null;
+
+  busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
+    switch (fieldname) {
+      case 'map':
+        try { fieldMap = JSON.parse(val); } catch (ex) {  }
+        break;
+      case 'type':
+        if (val == 'xlsx' || val == 'csv') { fileType = val }
+      break;
+      case 'namefield':
+          nameField = val;
+      break;
+			case 'sheet':
+				sheetName = val;
+      default:
+      // do nothing
+      break;
+    }
+  });
+
+	var mapRow = function(row) {
+		var r = {};
+
+		fieldMap.forEach((mapEnt) => { r[mapEnt[1]] = row[mapEnt[0]] });
+		r.names = r[nameField].replace(',',' ').replace('.',' ').replace('  ',' ').toLowerCase().split(' ');
+		return r;
+	};
+
+
+  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+		console.log(`receiving file ${filename}`);
+    let fn = `${tempy.file()}.xlsx`;
+    let f = fs.createWriteStream(fn);
+
+    file.on('end', () => {
+				console.log(`file ${fn} received, ${f.bytesWritten}, xlsx processing starting`);
+        book = xlsx.readFile(fn);
+        fs.unlinkSync(fn);
+    });
+
+			file.pipe(f);
+  });
+
+  busboy.on('finish', () => {
+    if (fieldMap && fileType && nameField && sheetName) {
+
+			if (fileType == 'csv') { res.statusCode = 500; res.end('CSV not implemented yet') }
+			else {
+				var sheet = book.Sheets[sheetName];
+				var docArray = xlsx.utils.sheet_to_json(sheet).map(mapRow);
+				console.log(`xlsx processing done, ${docArray.length} rows`);
+				docArray.forEach((doc) => writePromises.push(db.writeDoc(`data${dataNumber}`, doc)));
+
+				console.log(`request processing done, async waiting on db commit for ${writePromises.length} documents`)
+	      res.statusCode = 200;
+				res.write(`writing ${writePromises.length} documents\n`);
+	      Promise.all(writePromises).then(() => { res.end(`${writePromises.length} documents written`); console.log('db async wait done'); } );
+			}
+    } else {
+			console.log('param problem in request')
+      res.statusCode = 400;
+      res.end();
+    }
+  });
+
+  req.pipe(busboy);
+});
+
+module.exports = api;
--- api.js
+++ api.js
@@ -1,145 +1,28 @@
-const Busboy = require('busboy');
+
 const express = require('express');
 const api = express();
 const db = require('./db')
 const outputs = require('./outputs');
-const tempy = require('tempy');
-const fs = require('fs');
-const xlsx = require('xlsx');
+const dataRouter = require('./api-data');
 
-function tokenizeName(name) {
-	// eliminate punctuation - well just commas to begin
-	name.replace(',',' ');
 
-	// tokenize on space
-	return name.split(' ').map(s => s.toLowerCase());
-}
-
-const setup = function(req, res, next) {
-  res.setHeader('content-type', 'text/xml');
-  next();
-}
-
-api.use(setup);
 
 api.get('/status', (req, res) => {
   res.end('something');
 })
 
-api.get('/employees', (req, res) => {
-    let empColl = db.table('employeeout');
-    let tx = new outputs.DocToCsvTransformStream({objectMode : true});
 
-    res.setHeader('Content-type', 'text/csv');
-    empColl.on('end', () => res.end());
-    empColl.pipe(tx).pipe(res);
+api.delete('/data/:number([1-9]{1})', (req, res) => {
+	 	db.deleteCollection(`data${req.params.number}`)
+		.then(res.end(`deleted data${req.params.number}`))
+		.catch((e) => {
+			res.statusCode = 500;
+			res.end(e);
+		});
 });
 
-api.get('/users', (req, res) => {
-  let users = db.table('emailout');
-  let tx = new outputs.DocToCsvTransformStream({objectMode : true});
-
-  res.setHeader('Content-type', 'text/csv');
-  users.on('end', () => res.end());
-  users.pipe(tx).pipe(res);
-});
-
-function dieWithDetails(details) {
-  res.statusCode = 400;
-  res.end(details);
-}
-
-api.delete('/data/:number', (req, res) => {
-	var dataNumber = req.params.number;
-	if (!/^[1-9]{1}$/.test(dataNumber) || dataNumber < 1 || dataNumber > 9) {
-    res.statusCode = 400;
-    res.end();
-  } else {
-  	 	db.deleteCollection(`data${dataNumber}`).then(res.end(`deleted data${dataNumber}`))
-  }
-});
-
-api.post('/data/:number', (req, res) => {
-  var busboy = new Busboy({ headers : req.headers });
-	var book;
-  var writePromises = [];
-  var dataNumber = req.params.number;
-  var fieldMap = null;
-  var fileType = null;
-  var nameField = null;
-	var sheetName = null;
-
-  if (!/^[1-9]{1}$/.test(dataNumber) || dataNumber < 1 || dataNumber > 9) {
-    res.statusCode = 400;
-    res.end();
-  }
-
-  busboy.on('field', (fieldname, val, fieldnameTruncated, valTruncated, encoding, mimetype) => {
-    switch (fieldname) {
-      case 'map':
-        try { fieldMap = JSON.parse(val); } catch (ex) {  }
-        break;
-      case 'type':
-        if (val == 'xlsx' || val == 'csv') { fileType = val }
-      break;
-      case 'namefield':
-          nameField = val;
-      break;
-			case 'sheet':
-				sheetName = val;
-      default:
-      // do nothing
-      break;
-    }
-  });
-
-	var mapRow = function(row) {
-		var r = {};
-
-		fieldMap.forEach((mapEnt) => { r[mapEnt[1]] = row[mapEnt[0]] });
-		r.names = r[nameField].replace(',',' ').replace('.',' ').replace('  ',' ').toLowerCase().split(' ');
-		return r;
-	};
-
-
-  busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
-		console.log(`receiving file ${filename}`);
-    let fn = `${tempy.file()}.xlsx`;
-    let f = fs.createWriteStream(fn);
-
-    file.on('end', () => {
-				console.log(`file ${fn} received, ${f.bytesWritten}, xlsx processing starting`);
-        book = xlsx.readFile(fn);
-        fs.unlinkSync(fn);
-    });
-
-			file.pipe(f);
-  });
-
-  busboy.on('finish', () => {
-    if (fieldMap && fileType && nameField && sheetName) {
-
-			if (fileType == 'csv') { res.statusCode = 500; res.end('CSV not implemented yet') }
-			else {
-				var sheet = book.Sheets[sheetName];
-				var docArray = xlsx.utils.sheet_to_json(sheet).map(mapRow);
-				console.log(`xlsx processing done, ${docArray.length} rows`);
-				docArray.forEach((doc) => writePromises.push(db.writeDoc(`data${dataNumber}`, doc)));
-
-				console.log(`request processing done, async waiting on db commit for ${writePromises.length} documents`)
-	      res.statusCode = 200;
-				res.write(`writing ${writePromises.length} documents\n`);
-	      Promise.all(writePromises).then(() => { res.end(`${writePromises.length} documents written`); console.log('db async wait done'); } );
-			}
-    } else {
-			console.log('param problem in request')
-      res.statusCode = 400;
-      res.end();
-    }
-  });
-
-  req.pipe(busboy);
-});
+// POST router for /data
+api.use('/data', dataRouter);
 
 db.connect('mongodb://localhost:27017', 'temnames')
 .then(api.listen(8080,() => {}))
