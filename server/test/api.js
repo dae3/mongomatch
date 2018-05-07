1a512c203ce2e8fd294bd5f949c1d63f89760321
Daniel Everett <deverett@gmail.com>
2018-05-07 19:28:15 +1000

    API test suite done

--- server/test/api.js
+++ server/test/api.js
@@ -5,6 +5,8 @@ const stream = require('stream');
 const chai = require('chai');
 const sinon = require('sinon');
 const expect = chai.expect;
+const fs = require('fs');
+const through2 = require('through2');
 
 describe('base api', () => {
   const db = {
@@ -22,13 +24,15 @@ describe('base api', () => {
 
   const URL = `http://localhost:${process.env.API_PORT}`;
 
+	const textToObjTransform = through2.obj(function(ch,enc,cb) {
+	// naive implementation requires entire string on 1 line
+		this.push(JSON.parse(ch));
+		cb();
+	});
+	const fakePipeline = [ { $lookup: { from: ''} }];
+
   beforeEach(() => {
 
-  });
-
-
-  it('is just a test', function() {
-    expect(true).to.be.true;
   });
 
   it('should return status', function(done) {
@@ -62,10 +66,69 @@ describe('base api', () => {
 		req.get(`${URL}/crossmatch/somefrom/someto`, function(err, res, body) {
 				expect(res.statusCode).to.equal(200);
 				sinon.assert.calledWith(db.promisfyReadJson, './crossmatch.json');
-				fakePipeline[0].$lookup.from = 'nothis';
 				sinon.assert.calledWith(db.promisifyAggregateCollection, 'somefrom', fakePipeline);
 				done();
 		});
-  })
+  });
+
+	it('should score two collections', function(done) {
+
+		const fakeDocsStream = fs.createReadStream('./test/matchsample.json').pipe(textToObjTransform);
+		db.promisfyReadJson = sinon.fake.resolves(fakePipeline);
+		db.promisifyAggregateCollection = sinon.fake.resolves(fakeDocsStream);
+
+		req.get(`${URL}/scoreCrossmatch/somefrom/someto`, function(err, res, body) {
+				expect(res.statusCode).to.equal(200);
+				var bodyObj = JSON.parse(body)[0];
+				expect(bodyObj).to.have.property('matchedNames');
+				bodyObj.matchedNames.map((mn)=>{
+								expect(mn).to.have.property('score').a('number')
+				});
+				done();
+		});
+	});
+
+	it('should return a collection', function(done) {
+		const textToObjTransform = through2.obj(function(ch,enc,cb) {
+			// naive implementation requires entire string on 1 line
+			this.push(JSON.parse(ch));
+			cb();
+		});
+		const fakeCollection = {
+			find: function() {
+				return fs
+					.createReadStream('./test/matchsample.json')
+					.pipe(textToObjTransform);
+			}
+		};
+		db.promiseTable = sinon.fake.resolves(fakeCollection);
+		req.get(`${URL}/collection/something`, function(err, res, body) {
+			expect(res.statusCode).to.equal(200);
+			sinon.assert.calledWith(db.promiseTable,'something');
+			done();
+		});
+	});
+
+	it('should delete a collection', function(done) {
+		db.deleteCollection = sinon.fake.resolves();
+		req.delete(`${URL}/data/1`, function(err, res, body) {
+			sinon.assert.calledWith(db.deleteCollection, 'data1');
+			expect(res.statusCode).to.equal(200);
+			done();
+		});
+	});
+
+	it('should list all collections', function(done) {
+		db.getAllCollections = sinon.fake.resolves(
+			[ { s: { name: 'n1' } }, { s: { name : 'n2' } } ]
+		);
+
+		req.get(`${URL}/collections`, function(err, res, body) {
+			sinon.assert.calledOnce(db.getAllCollections);
+			expect(res.statusCode).to.equal(200);
+			done();
+		});
+
+	});
 
 });
